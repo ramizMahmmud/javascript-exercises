f9ac2d6de7ad9d58c1aeb184120a10a235ebacc6
ramizMahmmud <89058495+ramizMahmmud@users.noreply.github.com>
2023-09-11 11:30:34 +0600

    Do fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    if(firstNum<0 || secondNum <0 || typeof(firstNum) != "number" || typeof(secondNum)!= "number"){
+        return "ERROR"
+    }
+    let sum;
+    sum = 0;
+    let temp;
+    if(firstNum > secondNum){
+        temp = firstNum;
+        firstNum = secondNum;
+        secondNum =temp;
+    }
+    for(let i = firstNum; i<= secondNum; i ++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
