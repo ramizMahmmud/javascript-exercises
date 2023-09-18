3d8a1c810f99b5deb99a88c3356451006bba8fe7
ramizMahmmud <89058495+ramizMahmmud@users.noreply.github.com>
2023-09-18 10:13:15 +0600

    Do exercise 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  total = 0;
+  for(let i = 0; i< arr.length; i++){
+    total += arr[i];
+  }
+	return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  product = 1;
+  for(let i = 0; i< arr.length; i++)
+  {
+    product *= arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a **b;
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  fact = 1;
+  if (a === 0) return 1;
+  else {
+    for(let i = a; i>=1; i--){
+      fact *= i;
+    }
+    
+  }
+  return fact;
 	
 };
 
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,35 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    function isAlphaNumeric(str){
+        let code;
+        for(let i = 0; i<str.length; i++){
+            code = str.charCodeAt();
+            if(!(code>47 && code <58) &&
+            !(code > 64 && code < 91) &&
+            !(code > 96 && code < 123)){
+                return false;
+    
+            }
+            return true;
+        }
+    }
+    
+    let b = '';
+    let c = '';
+    for(let i = 0; i< a.length; i++){
+        if (isAlphaNumeric(a[i])){
+            b += a[i];
+        }
+    }
+    
+    b = b.toLocaleLowerCase();
+    c = b.split('').reverse().join('');
+    return b ===c;
 };
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
