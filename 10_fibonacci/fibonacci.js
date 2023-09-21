bc4793026f6fa9644e45154103cd94cf7269e528
ramizMahmmud <89058495+ramizMahmmud@users.noreply.github.com>
2023-09-21 12:16:47 +0600

    Do ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n<1) return 'OOPS'
+    if(n == 1 || n == 2) return 1;
+    let temp,resultBefore, result;
+    result =1;
+    resultBefore = 1;
+    for(let i =1; i <= n-2; i++){
+        temp = result;
+        result += resultBefore;
+        resultBefore = temp;
+    }
+    return result;
 };
 
 // Do not edit below this line
