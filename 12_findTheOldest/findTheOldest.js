d386fa3b46589c71901b027d71aeb3811760a295
ramizMahmmud <89058495+ramizMahmmud@users.noreply.github.com>
2023-09-22 00:07:50 +0600

    Do ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    function howOld(yearOfBirth, yearOfDeath){
+        return yearOfDeath - yearOfBirth;
+    };
+    let yearOfBirths = [];
+    let yearOfDeaths = [];
+    people.forEach(element => {
+        if(element.yearOfDeath == null){
+            element.yearOfDeath = 2023;
+        }
+        yearOfBirths.push(element.yearOfBirth);
+        yearOfDeaths.push(element.yearOfDeath);
+        
+    }); 
+    let len = people.length;
+    let ages = [];
+    let oldest;
+    let age;
+    for(let i = 0; i<len; i++){
+       age = howOld(yearOfBirths[i],yearOfDeaths[i]);
+        ages.push(age);
+    }
+    oldest = Math.max(...ages);
+   return people[ages.indexOf(oldest)];
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
