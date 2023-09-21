641b9f67ae330dd945e2bf3ff2d0b5fd97860bad
ramizMahmmud <89058495+ramizMahmmud@users.noreply.github.com>
2023-09-21 12:47:44 +0600

    Do ex 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let arr = [];
+    books.forEach(element => {
+        arr.push(element.title);
+    }); 
+    return arr;
 };
 
 // Do not edit below this line
